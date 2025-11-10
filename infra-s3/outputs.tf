0640ef132430876344ea03fc6a7ba033228c5b27
106xayt <arstedist@gmail.com>
2025-11-10 16:54:59 +0000

    fix: deduplicate outputs.tf and versions.tf

--- infra-s3/outputs.tf
+++ infra-s3/outputs.tf
@@ -12,19 +12,5 @@ output "lifecycle_rule_id" {
   description = "ID of the lifecycle rule that targets midlertidig/"
   value       = "midlertidig-rule"
 }
-output "bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.analysis.bucket
-}
-
-output "bucket_region" {
-  description = "Region where the bucket is created"
-  value       = var.region
-}
-
-output "lifecycle_rule_id" {
-  description = "ID of the lifecycle rule that targets midlertidig/"
-  value       = "midlertidig-rule"
-}
 
 # Note: lifecycle_resource_id removed to avoid deprecated attribute warnings.
